28af8e6dcadab0806889981a5a65fd64cdd562fb
Luis D. Pedrosa <luis.pedrosa@usc.edu>
2012-02-01 17:03:30 -0800

    Notify when indirect function calls are detected.

--- lib/SPA/CG.cpp
+++ lib/SPA/CG.cpp
@@ -5,6 +5,9 @@
 #include "spa/CG.h"
 
 #include "llvm/Instructions.h"
+#include "llvm/Analysis/DebugInfo.h"
+
+#include "cloud9/Logger.h"
 
 using namespace llvm;
 
@@ -14,10 +17,18 @@ namespace SPA {
 		for ( CFG::iterator it = cfg.begin(), ie = cfg.end(); it != ie; it++ ) {
 			// Check for CallInst or InvokeInst.
 			if ( const InvokeInst *ii = dyn_cast<InvokeInst>( *it ) ) {
+				if ( ! ii->getCalledFunction() ) {
+					DILocation *l = (DILocation *) (*it)->getDebugLoc().getAsMDNode( (*it)->getContext() );
+					CLOUD9_INFO( "Indirect function call found in function " << (*it)->getParent()->getParent()->getName().str() << " in file " << l->getFilename().str() << ":" << l->getLineNumber() << "." );
+				}
 				functions.insert( ii->getCalledFunction() );
 				callers[ii->getCalledFunction()].insert( *it );
 			}
 			if ( const CallInst *ci = dyn_cast<CallInst>( *it ) ) {
+				if ( ! ci->getCalledFunction() ) {
+					DILocation *l = (DILocation *) (*it)->getDebugLoc().getAsMDNode( (*it)->getContext() );
+					CLOUD9_INFO( "Indirect function call found in function " << (*it)->getParent()->getParent()->getName().str() << " in file " << l->getFilename().str() << ":" << l->getLineNumber() << "." );
+				}
 				functions.insert( ci->getCalledFunction() );
 				callers[ci->getCalledFunction()].insert( *it );
 			}
