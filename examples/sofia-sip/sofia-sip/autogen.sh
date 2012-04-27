be4cbb30866eb1f942e6ce793cdf42c7e7e1e3bc
Luis D. Pedrosa <luis.pedrosa@usc.edu>
2012-04-26 17:17:08 -0700

    Got SIP clients to do standard compile.

--- examples/sofia-sip/sofia-sip/autogen.sh
+++ examples/sofia-sip/sofia-sip/autogen.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 set -x
+AUTOMAKE=/usr/bin/automake ACLOCAL=/usr/bin/aclocal
 AUTOMAKE=${AUTOMAKE:-automake-1.9} ACLOCAL=${ACLOCAL:-aclocal-1.9}
 export AUTOMAKE ACLOCAL
 ${AUTORECONF:-autoreconf} -i
