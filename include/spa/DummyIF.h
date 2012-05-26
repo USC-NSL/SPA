91349354fdfba8c407aaa1b7fe2ae7c7889fbf27
Luis D. Pedrosa <luis.pedrosa@usc.edu>
2012-05-25 19:01:22 -0700

    Added a dummy instruction filter to quickly enable pass all filtering.

---
+++ include/spa/DummyIF.h
@@ -0,0 +1,22 @@
+/*
+ * SPA - Systematic Protocol Analysis Framework
+ */
+
+#ifndef __DummyIF_H__
+#define __DummyIF_H__
+
+#include <set>
+
+#include <llvm/Instruction.h>
+
+#include <spa/InstructionFilter.h>
+
+namespace SPA {
+	class DummyIF : public InstructionFilter {
+	public:
+		DummyIF() { }
+		bool checkInstruction( llvm::Instruction *instruction ) { return true; }
+	};
+}
+
+#endif // #ifndef __DummyIF_H__
