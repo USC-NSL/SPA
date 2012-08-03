f4073de5907ac6dad31d20bd5a4d9e4f66379760
Luis D. Pedrosa <luis.pedrosa@usc.edu>
2012-08-02 18:59:50 -0700

    Disabled compile optimizations in PJSIP. Updated run script.

--- examples/pjsip/spa-client.sh
+++ examples/pjsip/spa-client.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
-time spa --stand-alone --libc=uclibc --posix-runtime --max-stp-time=1 --max-instruction-time=1 --client pjsip-apps/bin/pjsua-x86_64-unknown-linux-gnu.bc 2>&1 | tee output.client.log
+rm -rf pjsip-apps/bin/klee-*
+time spa --stand-alone --libc=uclibc --posix-runtime --max-stp-time=1 --max-instruction-time=1 --client pjsip-apps/bin/pjsua-x86_64-unknown-linux-gnu.bc 2>&1 | tee client.log
