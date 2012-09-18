54e42e828d092ad457c5bb6a30b4fa33a18cd424
Luis D. Pedrosa <luis.pedrosa@usc.edu>
2012-09-17 17:10:03 -0700

    Implemented PathLoader bookmarking.

--- include/spa/PathLoader.h
+++ include/spa/PathLoader.h
@@ -8,17 +8,24 @@
 #include <spa/Path.h>
 #include <spa/PathFilter.h>
 
+#include <fstream>
+
 namespace SPA {
 	class PathLoader {
 	private:
-		std::istream &input;
-		int lineNumber;
+		std::ifstream &input;
+		unsigned long lineNumber;
 		PathFilter *filter;
 
+		unsigned long savedLN;
+		std::streampos savedPos;
+
 	public:
-		PathLoader( std::istream &_input ) : input( _input ), lineNumber( 0 ), filter( NULL ) {}
+		PathLoader( std::ifstream &_input ) : input( _input ), lineNumber( 0 ), filter( NULL ), savedLN( 0 ), savedPos( 0 ) {}
 		void setFilter( PathFilter *_filter ) { filter = _filter; }
-		void restart() { input.seekg( 0 ); lineNumber = 0; }
+		void restart() { input.clear(); input.seekg( 0, std::ios::beg ); lineNumber = 0; }
+		void save() { savedPos = input.tellg(); savedLN = lineNumber; }
+		void load() { input.clear(); input.seekg( savedPos, std::ios::beg ); lineNumber = savedLN; }
 
 		Path *getPath();
 	};
