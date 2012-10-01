2169e07cb579a7aa3d52807d504621a9f29fc3f7
Luis D. Pedrosa <luis.pedrosa@usc.edu>
2012-10-01 15:22:49 -0700

    Forked netcalc-trn.

---
+++ examples/netcalc-trn/netcalc-client.c
@@ -0,0 +1,147 @@
+#include <strings.h>
+#include <assert.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+
+#include <list>
+#include <iostream>
+
+#include <spa/spaRuntime.h>
+
+#include "netcalc.h"
+
+#define DEFAULT_HOST	"localhost"
+#define HOST_PARAM		"--host"
+
+int sock;
+struct addrinfo *server;
+
+std::list<nc_value_t> stack;
+
+// Executes a single query on the remote server.
+nc_value_t executeQuery( nc_operator_t op, nc_value_t arg1, nc_value_t arg2 ) {
+	spa_api_input_var( op );
+	spa_api_input_var( arg1 );
+	spa_api_input_var( arg2 );
+
+	spa_seed_var( 1, op, NC_ADDITION );
+	spa_seed_var( 1, arg1, 1 );
+	spa_seed_var( 1, arg2, 2 );
+
+	spa_seed_var( 2, op, NC_SUBTRACTION );
+	spa_seed_var( 2, arg1, 2 );
+	spa_seed_var( 2, arg2, 1 );
+
+	assert( op >= 0 && op < NC_OPERATOR_END && "Invalid operator in query.");
+// 	assert( (op != NC_DIVISION || arg2 != 0) && "Division by zero.");
+// 	assert( (op != NC_MODULO || arg2 != 0) && "Modulo by zero.");
+
+	nc_query_t query;
+	query.op = op;
+	query.arg1 = arg1;
+	query.arg2 = arg2;
+
+	spa_msg_output_var( query );
+
+	// Send query.
+	assert( sendto( sock, &query, sizeof( query ), 0, server->ai_addr, server->ai_addrlen ) == sizeof( query ) );
+	// Get response.
+	nc_response_t response;
+	assert( recv( sock, &response, sizeof( response ), 0 ) == sizeof( response ) );
+	// Output operation.
+	if ( response.err == NC_OK ) {
+		std::cerr << "	" << query.arg1 << " " << getOpName( query.op ) << " " << query.arg2 << " = " << response.value << std::endl;
+	} else {
+		std::cerr << "Error: " << getErrText( response.err ) << std::endl;
+	}
+	assert( response.err == NC_OK && "Query failed." );
+
+	return response.value;
+}
+
+void __attribute__((used)) SpaExecuteQueryEntry() {
+	spa_api_entry();
+
+// 	struct addrinfo hints;
+// 	bzero( &hints, sizeof( hints ) );
+// 	hints.ai_family = AF_INET;
+// 	hints.ai_socktype = SOCK_DGRAM;
+// 	hints.ai_protocol = IPPROTO_UDP;
+// 	assert( ! getaddrinfo( DEFAULT_HOST, NULL, &hints, &server ) );
+// 	((struct sockaddr_in *) server->ai_addr)->sin_port = htons( NETCALC_UDP_PORT );
+// 	assert( (sock = socket( server->ai_family, server->ai_socktype, server->ai_protocol )) >= 0 && "Host could not be resolved." );
+
+	executeQuery( NC_ADDITION, 0, 0 );
+}
+
+// Handle an RPN operation (either an operator or a number to push).
+void handleRPNOp( std::string rpnOp ) {
+	nc_operator_t ncOp;
+
+	// Check whether operator or number.
+	if ( (ncOp = getOpByName( rpnOp )) == NC_OPERATOR_END ) { // Not a known operator. Assume number.
+		stack.push_back( atoi( rpnOp.c_str() ) );
+	} else {
+		assert( stack.size() >= 2 && "Not enough data in stack to run an operation." );
+		// Pop arguments.
+		nc_value_t arg2 = stack.back();
+		stack.pop_back();
+		nc_value_t arg1 = stack.back();
+		stack.pop_back();
+		// Push result.
+		stack.push_back( executeQuery( ncOp, arg1, arg2 ) );
+	}
+}
+
+// Output stack elements.
+void dumpStack() {
+	for ( std::list<nc_value_t>::iterator it = stack.begin(), ie = stack.end(); it != ie; it++ ) {
+		if ( it != stack.begin() )
+			std::cout << "	";
+		std::cout << *it;
+	}
+	std::cout << std::endl;
+}
+
+// Execute a list of RPN operations and output result.
+void executeRPN( std::list<std::string> rpn ) {
+	for ( std::list<std::string>::iterator it = rpn.begin(), ie = rpn.end(); it != ie; it++ )
+		handleRPNOp( *it );
+	dumpStack();
+}
+
+int main( int argc, char **argv ) {
+	std::list<std::string> args( argv, argv + argc );
+
+	// Remove program name.
+	args.pop_front();
+
+	const char *host = DEFAULT_HOST;
+	// Check if user specified a host.
+	if ( args.size() > 2 && args.front() == HOST_PARAM ) {
+		args.pop_front();
+		host = args.front().c_str();
+		args.pop_front();
+	}
+	std::cerr << "Sending queries to " << host << "." << std::endl;
+
+	struct addrinfo hints, *result;
+	bzero( &hints, sizeof( hints ) );
+	hints.ai_family = AF_INET;
+	hints.ai_socktype = SOCK_DGRAM;
+	hints.ai_protocol = IPPROTO_UDP;
+	assert( ! getaddrinfo( host, NULL, &hints, &result ) );
+
+	for ( server = result; server; server = server->ai_next ) {
+		((struct sockaddr_in *) result->ai_addr)->sin_port = htons( NETCALC_UDP_PORT );
+		if ( (sock = socket( server->ai_family, server->ai_socktype, server->ai_protocol )) < 0 )
+			continue;
+		break;
+	}
+	assert( sock >= 0 && "Host could not be resolved." );
+
+	executeRPN( args );
+
+	freeaddrinfo( result );
+}
---
+++ examples/netcalc-trn/netcalc.h
@@ -0,0 +1,64 @@
+#define NETCALC_UDP_PORT 3141
+
+typedef enum {
+	NC_ADDITION = 0,
+	NC_SUBTRACTION,
+	NC_MUTIPLICATION,
+	NC_DIVISION,
+	NC_MODULO,
+
+	NC_OPERATOR_END
+} nc_operator_t;
+
+// Convert operator to it name.
+std::string getOpName( nc_operator_t op ) {
+	switch ( op ) {
+		case NC_ADDITION :		return "+";
+		case NC_SUBTRACTION :	return "-";
+		case NC_MUTIPLICATION :	return "*";
+		case NC_DIVISION :		return "/";
+		case NC_MODULO :		return "%";
+		default :				return "[Unknown Operator]";
+	}
+}
+
+// Convert an operator name to the operator enum.
+nc_operator_t getOpByName( std::string name ) {
+	if ( "+" == name ) return NC_ADDITION;
+	if ( "-" == name ) return NC_SUBTRACTION;
+	if ( "*" == name ) return NC_MUTIPLICATION;
+	if ( "/" == name ) return NC_DIVISION;
+	if ( "%" == name ) return NC_MODULO;
+	return NC_OPERATOR_END;
+}
+
+typedef long nc_value_t;
+
+typedef struct {
+	nc_operator_t op;
+	nc_value_t arg1;
+	nc_value_t arg2;
+} nc_query_t;
+
+typedef enum {
+	NC_OK = 0,
+	NC_BADINPUT,
+	NC_DIV0,
+
+	NC_ERROR_END
+} nc_error_t;
+
+// Convert an error enum to a human readable description.
+std::string getErrText( nc_error_t err ) {
+	switch ( err ) {
+		case NC_OK :		return "OK.";
+		case NC_BADINPUT :	return "Bad input.";
+		case NC_DIV0 :		return "Division by zero.";
+		default :			return "Unknown error.";
+	}
+}
+
+typedef struct {
+	nc_error_t err;
+	nc_value_t value;
+} nc_response_t;
