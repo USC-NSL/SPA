45596766f34bd2e570a3e6125805eaf57c46ec7e
Ari Fogel <arifogel@ucla.edu>
2013-01-04 14:05:45 -0800

    debugging problems with llvm and openflow instrumentation..

--- examples/openflow/instrumentation/test.c
+++ examples/openflow/instrumentation/test.c
@@ -4,6 +4,23 @@
 #include "vconn.h"
 #include "ofpbuf.h"
 
+static bool
+is_admitted_msg(const struct ofpbuf *b)
+{
+    struct ofp_header *oh = b->data;
+    uint8_t type = oh->type;
+    return !(type < 32                                                                                                                      
+             && (1u << type) & ((1u << OFPT_HELLO) |
+                                (1u << OFPT_ERROR) |
+                                (1u << OFPT_ECHO_REQUEST) |
+                                (1u << OFPT_ECHO_REPLY) |
+                                (1u << OFPT_VENDOR) |
+                                (1u << OFPT_FEATURES_REQUEST) |
+                                (1u << OFPT_FEATURES_REPLY) |
+                                (1u << OFPT_GET_CONFIG_REQUEST) |
+                                (1u << OFPT_GET_CONFIG_REPLY) |
+                                (1u << OFPT_SET_CONFIG)));
+}
 
 int main(int argc, char** argv) {
    char crap[1000];
