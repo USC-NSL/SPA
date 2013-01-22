9ed78e47f421ac3210ee8690e3c26ab2f96143fa
Luis D. Pedrosa <luis.pedrosa@usc.edu>
2013-01-21 18:02:31 -0800

    Added a test build to PJSIP for SPA testing. Fixed PJSIP build scripts.

--- examples/pjsip/build.sh
+++ examples/pjsip/build.sh
@@ -1,17 +1,53 @@
 #!/bin/sh
 
-make -skj distclean
-./configure-dbg
-make -skj distclean
-make dep
-make -skj8
-make -sk
-cp pjsip-apps/bin/pjsua-x86_64-unknown-linux-gnu.bc pjsua.dbg.bc
+# Die on failure
+set -e
 
 make -skj distclean
-./configure-llvm
-make -skj distclean
+rm -f pjsua.dbg.exe
+./configure-dbg
 make dep
-make -skj8
-make -sk
+make -k || echo
+touch pjmedia/bin/pjmedia-test-x86_64-unknown-linux-gnu
+make -k || echo
+touch pjsip/bin/pjsip-test-x86_64-unknown-linux-gnu
+make -k || echo
+mkdir -p pjsip-apps/bin/samples/x86_64-unknown-linux-gnu
+touch pjsip-apps/bin/samples/x86_64-unknown-linux-gnu/pjsip-perf
+touch pjsip-apps/bin/samples/x86_64-unknown-linux-gnu/simpleua
+touch pjsip-apps/bin/samples/x86_64-unknown-linux-gnu/siprtp
+touch pjsip-apps/bin/samples/x86_64-unknown-linux-gnu/sipstateless
+touch pjsip-apps/bin/samples/x86_64-unknown-linux-gnu/stateful_proxy
+touch pjsip-apps/bin/samples/x86_64-unknown-linux-gnu/stateless_proxy
+touch pjsip-apps/bin/samples/x86_64-unknown-linux-gnu/playsine
+make
+cp pjsip-apps/bin/pjsua-x86_64-unknown-linux-gnu pjsua.dbg.exe
+
+make -skj distclean
+rm -f pjsua.test.exe
+./configure-test
+make dep
+make -k || echo
+touch pjmedia/bin/pjmedia-test-x86_64-unknown-linux-gnu
+make -k || echo
+touch pjsip/bin/pjsip-test-x86_64-unknown-linux-gnu
+make -k || echo
+mkdir -p pjsip-apps/bin/samples/x86_64-unknown-linux-gnu
+touch pjsip-apps/bin/samples/x86_64-unknown-linux-gnu/pjsip-perf
+touch pjsip-apps/bin/samples/x86_64-unknown-linux-gnu/simpleua
+touch pjsip-apps/bin/samples/x86_64-unknown-linux-gnu/siprtp
+touch pjsip-apps/bin/samples/x86_64-unknown-linux-gnu/sipstateless
+touch pjsip-apps/bin/samples/x86_64-unknown-linux-gnu/stateful_proxy
+touch pjsip-apps/bin/samples/x86_64-unknown-linux-gnu/stateless_proxy
+touch pjsip-apps/bin/samples/x86_64-unknown-linux-gnu/playsine
+make
+cp pjsip-apps/bin/pjsua-x86_64-unknown-linux-gnu pjsua.test.exe
+
+make -skj distclean
+rm -f pjsua.spa.bc
+./configure-llvm
+make dep
+make -k || echo
+touch pjlib/bin/pjlib-test-x86_64-unknown-linux-gnu
+make
 cp pjsip-apps/bin/pjsua-x86_64-unknown-linux-gnu.bc pjsua.spa.bc
