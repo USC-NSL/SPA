1dd763f4dcea969fb0cdc9d73c94c9413e7e1dd3
Luis D. Pedrosa <luis.pedrosa@usc.edu>
2013-01-26 01:19:45 -0800

    Updated OPTIONS response analysis script.

--- examples/sip-options.sh
+++ examples/sip-options.sh
@@ -2,6 +2,6 @@
 
 set -e
 
-spaBadInputs -client exosip.paths -server pjsip.paths -o sip-options.untested |& tee sip-options.untested.log
+time spaBadInputs -client exosip.paths -server pjsip.paths -p 8 -d sip-options.untested.debug -o sip-options.untested 2>&1 | tee -a sip-options.untested.log
 
-spaE2ETest sip-options.untested sip-options.tested pjsip/pjsua.test.exe exosip/spa-eXosip2/options-client.e2etest.exe |& tee sip-options.tested.log
+time spaE2ETest sip-options.untested sip-options.tested pjsip/pjsua.test.exe exosip/spa-eXosip2/options-client.e2etest.exe 2>&1 | tee -a sip-options.tested.log
