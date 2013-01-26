f7405cfb6250e7bb20bf79e24dacda8fe7ac497a
Luis D. Pedrosa <luis.pedrosa@usc.edu>
2013-01-26 01:02:26 -0800

    Implemented a test harness to analyze eXoSIP responses to OPTIONS messages.

--- examples/exosip/spa-eXosip2/server.c
+++ examples/exosip/spa-eXosip2/server.c
@@ -15,9 +15,7 @@ void __attribute__((noinline,used)) spa_HandleInvite() {
 
 
 	assert( eXosip_init() == OSIP_SUCCESS );
-// 	assert( eXosip_set_socket( IPPROTO_UDP, 1, 5060 ) == OSIP_SUCCESS );
-// 	assert( eXosip_set_socket( IPPROTO_UDP, socket( AF_INET, SOCK_DGRAM, 0 ), 5060 ) == OSIP_SUCCESS );
-	assert( eXosip_listen_addr( IPPROTO_UDP, "0.0.0.0", 5060, AF_INET, 0) == OSIP_SUCCESS );
+	assert( eXosip_listen_addr( IPPROTO_UDP, "0.0.0.0", 5061, AF_INET, 0) == OSIP_SUCCESS );
 
 // 	char message[SIP_MESSAGE_MAX_LENGTH + 1];
 // 	size_t len;
@@ -34,12 +32,21 @@ void __attribute__((noinline,used)) spa_HandleInvite() {
 			case EXOSIP_CALL_INVITE:
 				printf( "Incoming call.\n" );
 				break;
+			case EXOSIP_MESSAGE_NEW:
+				printf( "New request. Method: %s\n", e->request->sip_method );
+				if ( strcmp( "OPTIONS", e->request->sip_method ) == 0 ) {
+					assert( eXosip_options_send_answer ( e->tid, 200, NULL ) == OSIP_SUCCESS );
+				}
+				break;
 			default:
-				printf( "Other event.\n" );
+				printf( "Other event. Type: %d\n", e->type );
+				printf( "tid: %d, did: %d, rid: %d. cid: %d, sid: %d, nid: %d\n", e->tid, e->did, e->rid, e->cid, e->sid, e->nid );
+				printf( "request: %d, response: %d, ack: %d\n", !!e->request, !!e->response, !!e->ack );
 				break;
 		}
+		printf( "Info: %s\n", e->textinfo );
 		eXosip_event_free( e );
-	} while ( 0 );
+	} while ( 1 );
 	printf( "Done.\n" );
 	eXosip_quit();
 }
