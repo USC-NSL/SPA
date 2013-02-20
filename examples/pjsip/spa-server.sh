917f11b4f1d985499822aa17d7818dcc04d1f303
Luis D. Pedrosa <luis.pedrosa@usc.edu>
2013-02-19 17:22:43 -0800

    Created seperate test-case for PJSIP options client.

--- examples/pjsip/spa-server.sh
+++ examples/pjsip/spa-server.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
-rm -rf pjsip-apps/bin/klee-*
-time spa --stand-alone --libc=uclibc --posix-runtime --max-stp-time=1 --max-instruction-time=1 -init-values pjsip-options.in --server pjsua.spa.bc 2>&1 | tee pjsip.server.log
+rm -rf klee-*
+#time spa --stand-alone --libc=uclibc --posix-runtime --max-stp-time=1 --max-instruction-time=1 -init-values pjsip-options-client.in -path-file pjsip-options-client.paths --server pjsua.spa.bc 2>&1 | tee pjsip.server.log
+time spa --stand-alone --libc=uclibc --posix-runtime --max-stp-time=1 --max-instruction-time=1 -path-file pjsip.paths --server pjsua.spa.bc 2>&1 | tee pjsip.paths.log
