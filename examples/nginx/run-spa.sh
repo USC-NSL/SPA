58e135e0868e4fcc09ba8c6b640cd54d6baa61f2
Luis D. Pedrosa <luis.pedrosa@usc.edu>
2014-01-23 04:28:44 -0800

    Configured run-time per-instruction limits during SPA nginx analysis.

--- examples/nginx/run-spa.sh
+++ examples/nginx/run-spa.sh
@@ -2,6 +2,8 @@
 
 /usr/local/bin/spa/Release+Asserts/bin/spa \
 	--stand-alone --libc=uclibc --posix-runtime \
+	-max-instruction-time=5 \
+	-max-stp-time=5 \
 	--path-file=nginx.paths \
 	--server objs/nginx.bc \
 	2>&1 | tee nginx.paths.log
