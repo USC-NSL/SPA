079809abca490ed97b649cf2ddc0b95502e8f00d
Luis D. Pedrosa <luis.pedrosa@usc.edu>
2014-04-01 22:05:23 -0700

    Enabled debug symbols in spa.

--- build.sh
+++ build.sh
@@ -6,6 +6,6 @@ set -e
 [ -d /usr/local/src/stp/bin ] || (echo "Please build STP before proceeding."; exit 1)
 [ -d /usr/local/src/klee-uclibc/lib ] || (echo "Please build KLEE-uClibc before proceeding."; exit 1)
 
-[ -f Makefile.config ] || CXXFLAGS='-std=c++11' ./configure --with-llvmsrc=/usr/local/src/llvm --with-llvmobj=/usr/local/src/llvm/build --with-stp=/usr/local/src/stp/bin --with-uclibc=/usr/local/src/klee-uclibc --enable-posix-runtime
+[ -f Makefile.config ] || CXXFLAGS='-std=c++11 -g' CFLAGS='-g' ./configure --with-llvmsrc=/usr/local/src/llvm --with-llvmobj=/usr/local/src/llvm/build --with-stp=/usr/local/src/stp/bin --with-uclibc=/usr/local/src/klee-uclibc --enable-posix-runtime
 
 make -skj`grep -c processor /proc/cpuinfo` ENABLE_OPTIMIZED=1
