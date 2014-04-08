84fbc7f2f87ff4432923b9ad22ec29ef3c42c7e9
Luis D. Pedrosa <luis.pedrosa@usc.edu>
2014-04-07 22:51:48 -0700

    Adpated PJSIP build scripts to use clang.

--- examples/pjsip/build.sh
+++ examples/pjsip/build.sh
@@ -3,9 +3,9 @@
 # Die on failure
 set -e
 
-make -skj distclean
 rm -f pjsua.dbg.exe
 ./configure-dbg
+make -skj distclean
 make dep
 make -k || echo
 touch pjmedia/bin/pjmedia-test-x86_64-unknown-linux-gnu
@@ -23,9 +23,9 @@ touch pjsip-apps/bin/samples/x86_64-unknown-linux-gnu/playsine
 make
 cp pjsip-apps/bin/pjsua-x86_64-unknown-linux-gnu pjsua.dbg.exe
 
-make -skj distclean
 rm -f pjsua.test.exe
 ./configure-test
+make -skj distclean
 make dep
 make -k || echo
 touch pjmedia/bin/pjmedia-test-x86_64-unknown-linux-gnu
@@ -43,11 +43,24 @@ touch pjsip-apps/bin/samples/x86_64-unknown-linux-gnu/playsine
 make
 cp pjsip-apps/bin/pjsua-x86_64-unknown-linux-gnu pjsua.test.exe
 
-make -skj distclean
 rm -f pjsua.spa.bc
 ./configure-llvm
+make -skj distclean
 make dep
 make -k || echo
-touch pjlib/bin/pjlib-test-x86_64-unknown-linux-gnu
-make
-cp pjsip-apps/bin/pjsua-x86_64-unknown-linux-gnu.bc pjsua.spa.bc
+llvm-link -o pjsua.spa.bc \
+	pjsip-apps/build/output/pjsua-x86_64-unknown-linux-gnu/main.o \
+	pjsip-apps/build/output/pjsua-x86_64-unknown-linux-gnu/pjsua_app.o \
+	pjsip/lib/libpjsua-x86_64-unknown-linux-gnu.a \
+	pjsip/lib/libpjsip-ua-x86_64-unknown-linux-gnu.a \
+	pjsip/lib/libpjsip-simple-x86_64-unknown-linux-gnu.a \
+	pjsip/lib/libpjsip-x86_64-unknown-linux-gnu.a \
+	pjmedia/lib/libpjmedia-codec-x86_64-unknown-linux-gnu.a \
+	pjmedia/lib/libpjmedia-x86_64-unknown-linux-gnu.a \
+	pjmedia/lib/libpjmedia-audiodev-x86_64-unknown-linux-gnu.a \
+	pjnath/lib/libpjnath-x86_64-unknown-linux-gnu.a \
+	pjlib-util/lib/libpjlib-util-x86_64-unknown-linux-gnu.a \
+	third_party/lib/libresample-x86_64-unknown-linux-gnu.a \
+	third_party/lib/libmilenage-x86_64-unknown-linux-gnu.a \
+	third_party/lib/libsrtp-x86_64-unknown-linux-gnu.a \
+	pjlib/lib/libpj-x86_64-unknown-linux-gnu.a
