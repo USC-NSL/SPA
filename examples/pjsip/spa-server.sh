07dab2331912cd73f90885fd3004be1552cd4b00
Luis D. Pedrosa <luis.pedrosa@usc.edu>
2014-04-11 16:59:04 -0700

    Adapted PJSIP SPA running script.

--- examples/pjsip/spa-server.sh
+++ examples/pjsip/spa-server.sh
@@ -2,4 +2,4 @@
 
 rm -rf klee-*
 #time spa --stand-alone --libc=uclibc --posix-runtime --max-stp-time=1 --max-instruction-time=1 -init-values pjsip-options-client.in -path-file pjsip-options-client.paths --server pjsua.spa.bc 2>&1 | tee pjsip.server.log
-time spa --stand-alone --libc=uclibc --posix-runtime --max-stp-time=1 --max-instruction-time=1 -path-file pjsip.paths --server pjsua.spa.bc 2>&1 | tee pjsip.paths.log
+time ../../Release+Asserts/bin/spa --max-solver-time=1 --max-instruction-time=1 -path-file pjsip.paths --server pjsua.spa.bc 2>&1 | tee pjsip.paths.log
