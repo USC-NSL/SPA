93b88528870cef1318ae01ddb4df53f8aecdc52f
Luis D. Pedrosa <luis.pedrosa@usc.edu>
2014-06-10 16:31:28 -0700

    Changed spdylay build script to use clang.

--- examples/spdylay/build.sh
+++ examples/spdylay/build.sh
@@ -5,15 +5,15 @@ set -e
 rm -f libspdylay.bc libspdylay-fixed.bc libspdylay-dbg.o libspdylay-test.o libspdylay-test-fixed.o
 
 make -skj distclean || echo
-CC='llvm-gcc' CFLAGS='-DENABLE_SPA -DENABLE_KLEE -emit-llvm -use-gold-plugin -O0 -g' \
-CXX='llvm-g++' CXXFLAGS='-DENABLE_SPA -DENABLE_KLEE -emit-llvm -use-gold-plugin -O0 -g' \
+CC='clang' CFLAGS='-DENABLE_SPA -DENABLE_KLEE -emit-llvm -use-gold-plugin -O0 -g' \
+CXX='clang++' CXXFLAGS='-DENABLE_SPA -DENABLE_KLEE -emit-llvm -use-gold-plugin -O0 -g' \
 LD='llvm-ld' LDFLAGS='-disable-opt' \
-CPP='llvm-cpp' AR='echo' RANLIB='echo' ./configure \
+CPP='clang -E' AR='llvm-ar' RANLIB='llvm-ranlib' ./configure \
 	--disable-shared \
 	--enable-static \
 	--disable-xmltest --disable-largefile
 make -skj8
-llvm-ld -disable-opt -r lib/*.o -o libspdylay.bc
+llvm-link lib/*.o -o libspdylay.bc
 
 # make -skj clean
 # CC='llvm-gcc' CFLAGS='-DENABLE_SPA -DENABLE_KLEE -DSPA_FIXES -emit-llvm -use-gold-plugin -O0 -g' \
@@ -27,8 +27,7 @@ llvm-ld -disable-opt -r lib/*.o -o libspdylay.bc
 
 make -skj clean
 CFLAGS='-DENABLE_SPA -O0 -g' \
-CXXFLAGS='-DENABLE_SPA -O0 -g' \
-AR='echo' RANLIB='echo' ./configure \
+CXXFLAGS='-DENABLE_SPA -O0 -g' ./configure \
 	--disable-shared \
 	--enable-static
 make -skj8
@@ -45,8 +44,7 @@ ld -r lib/*.o -o libspdylay-test.o
 
 make -skj clean
 CFLAGS='-O0 -g' \
-CXXFLAGS='-O0 -g' \
-AR='echo' RANLIB='echo' ./configure \
+CXXFLAGS='-O0 -g' ./configure \
 	--disable-shared \
 	--enable-static
 make -skj8
