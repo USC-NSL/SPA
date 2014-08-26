bf7dc13183bdd4e889d17da73d5610efa9ee658d
Luis D. Pedrosa <luis.pedrosa@usc.edu>
2014-08-25 21:51:46 -0700

    Fixed PJSIP build.

--- examples/pjsip/build.sh
+++ examples/pjsip/build.sh
@@ -33,13 +33,34 @@ make -k || echo
 touch pjsip/bin/pjsip-test-x86_64-unknown-linux-gnu
 make -k || echo
 mkdir -p pjsip-apps/bin/samples/x86_64-unknown-linux-gnu
+touch pjsip-apps/bin/pjsystest-x86_64-unknown-linux-gnu
+touch pjsip-apps/bin/samples/x86_64-unknown-linux-gnu/aectest
+touch pjsip-apps/bin/samples/x86_64-unknown-linux-gnu/auddemo
+touch pjsip-apps/bin/samples/x86_64-unknown-linux-gnu/confsample
+touch pjsip-apps/bin/samples/x86_64-unknown-linux-gnu/encdec
+touch pjsip-apps/bin/samples/x86_64-unknown-linux-gnu/httpdemo
+touch pjsip-apps/bin/samples/x86_64-unknown-linux-gnu/icedemo
+touch pjsip-apps/bin/samples/x86_64-unknown-linux-gnu/jbsim
+touch pjsip-apps/bin/samples/x86_64-unknown-linux-gnu/latency
+touch pjsip-apps/bin/samples/x86_64-unknown-linux-gnu/level
+touch pjsip-apps/bin/samples/x86_64-unknown-linux-gnu/mix
+touch pjsip-apps/bin/samples/x86_64-unknown-linux-gnu/pcaputil
 touch pjsip-apps/bin/samples/x86_64-unknown-linux-gnu/pjsip-perf
+touch pjsip-apps/bin/samples/x86_64-unknown-linux-gnu/playfile
+touch pjsip-apps/bin/samples/x86_64-unknown-linux-gnu/playsine
+touch pjsip-apps/bin/samples/x86_64-unknown-linux-gnu/recfile
+touch pjsip-apps/bin/samples/x86_64-unknown-linux-gnu/resampleplay
+touch pjsip-apps/bin/samples/x86_64-unknown-linux-gnu/simple_pjsua
 touch pjsip-apps/bin/samples/x86_64-unknown-linux-gnu/simpleua
 touch pjsip-apps/bin/samples/x86_64-unknown-linux-gnu/siprtp
 touch pjsip-apps/bin/samples/x86_64-unknown-linux-gnu/sipstateless
 touch pjsip-apps/bin/samples/x86_64-unknown-linux-gnu/stateful_proxy
 touch pjsip-apps/bin/samples/x86_64-unknown-linux-gnu/stateless_proxy
-touch pjsip-apps/bin/samples/x86_64-unknown-linux-gnu/playsine
+make -k || echo
+touch pjsip-apps/bin/samples/x86_64-unknown-linux-gnu/stereotest
+touch pjsip-apps/bin/samples/x86_64-unknown-linux-gnu/streamutil
+touch pjsip-apps/bin/samples/x86_64-unknown-linux-gnu/strerror
+touch pjsip-apps/bin/samples/x86_64-unknown-linux-gnu/tonegen
 make
 cp pjsip-apps/bin/pjsua-x86_64-unknown-linux-gnu pjsua.test.exe
 
@@ -64,3 +85,6 @@ llvm-link -o pjsua.spa.bc \
 	third_party/lib/libmilenage-x86_64-unknown-linux-gnu.a \
 	third_party/lib/libsrtp-x86_64-unknown-linux-gnu.a \
 	pjlib/lib/libpj-x86_64-unknown-linux-gnu.a
+
+echo
+echo All done.
--- examples/pjsip/spa-client.sh
+++ examples/pjsip/spa-client.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
-time spa --stand-alone --libc=uclibc --posix-runtime --max-stp-time=1 --max-instruction-time=1 --client pjsip-apps/bin/pjsua-x86_64-unknown-linux-gnu.bc 2>&1 | tee client.log
+rm -rf klee-*
+
+time spa --max-solver-time=1 --max-instruction-time=1 --max-stp-time=1 --max-instruction-time=1 --client pjsua.spa.bc -path-file pjsip-client.paths 2>&1 | tee pjsip-client.paths.log
