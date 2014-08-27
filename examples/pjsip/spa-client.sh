921d1b5df631bc7cd8db4074d0583bf2160bcc6b
Luis D. Pedrosa <lpedrosa@mammoth.usc.edu>
2014-08-27 13:09:40 -0700

    Fixed PJSIP client analysis script.

--- examples/pjsip/spa-client.sh
+++ examples/pjsip/spa-client.sh
@@ -2,4 +2,4 @@
 
 rm -rf klee-*
 
-time spa --max-solver-time=1 --max-instruction-time=1 --max-stp-time=1 --max-instruction-time=1 --client pjsua.spa.bc -path-file pjsip-client.paths 2>&1 | tee pjsip-client.paths.log
+time spa --max-solver-time=1 --max-instruction-time=1 --client pjsua.spa.bc -path-file pjsip-client.paths 2>&1 | tee pjsip-client.paths.log
