7878d93210266cd91f8a6eae24186fd7b121066d
Luis D. Pedrosa <luis.pedrosa@usc.edu>
2014-09-05 22:49:06 -0700

    Fixed parallel line tagging and added date of job start/finish.

--- scripts/parallel-spa-server.sh
+++ scripts/parallel-spa-server.sh
@@ -11,19 +11,23 @@ rm -f spa-client-*.paths spa-server-*.paths $3.joblog
 spaSplitPaths -i $2 -o 'spa-client-%04d.paths' -n 1000
 
 parallel --progress --joblog $3.joblog \
-  --controlmaster -v --tag --ungroup --linebuffer \
+  --controlmaster -v --tag --linebuffer \
   -S lpedrosa@spa1.pedrosa.3-a.net -S lpedrosa@spa2.pedrosa.3-a.net -S lpedrosa@spa3.pedrosa.3-a.net \
   --basefile $1 --transfer --return spa-server-{} --cleanup \
   "echo Basefile: $1; echo Transfer: {}; echo Return: spa-server-{}; echo ls:; ls; \
-  /home/lpedrosa/spa/Release+Asserts/bin/spa --path-file spa-server-{} -sender-paths {} --server $1" \
+  date '+Started: %s.%N (%c)'; \
+  /home/lpedrosa/spa/Release+Asserts/bin/spa --path-file spa-server-{} -sender-paths {} --server $1; \
+  date '+Finished: %s.%N (%c)';" \
   ::: spa-client-*.paths 2>&1 | tee $3.log
 
 # parallel --progress --joblog $3.joblog \
-#   --controlmaster -v --tag --ungroup --linebuffer \
+#   --controlmaster -v --tag --linebuffer \
 #   -S david@192.168.3.6 \
 #   --basefile $1 --transfer --return spa-server-{} --cleanup \
 #   "echo Basefile: $1; echo Transfer: {}; echo Return: spa-server-{}; echo ls:; ls; \
-#   /home/david/Projects/spa/Release+Asserts/bin/spa --path-file spa-server-{} -sender-paths {} --server $1" \
+#   date '+Started: %s.%N (%c)'; \
+#   /home/lpedrosa/spa/Release+Asserts/bin/spa --path-file spa-server-{} -sender-paths {} --server $1; \
+#   date '+Finished: %s.%N (%c)';" \
 #   ::: spa-client-*.paths 2>&1 | tee $3.log
 
 cat spa-server-*.paths > $3
