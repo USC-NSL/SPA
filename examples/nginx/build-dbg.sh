1c2529513406e8762599bb9423778a32797cf101
Luis D. Pedrosa <luis.pedrosa@usc.edu>
2014-09-12 03:13:15 -0700

    Recreated nginx build scripts to allow coexisting builds for dbg/llvm/test. Created nginx test build. Prepared nginx analysis pipeline Makefile.

--- examples/nginx/build-dbg.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-set -e
-
-[ -f zlib/Makefile ] || (cd zlib; ./configure-dbg; cd ..)
-[ -f Makefile ] || ./configure-dbg
-
-make -kj`grep -c processor /proc/cpuinfo`
--- examples/nginx/run-dbg.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-./objs/nginx -p .
