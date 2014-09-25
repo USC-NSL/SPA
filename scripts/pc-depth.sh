3a4be384df5afa23a20fe481a5f5956c5b9d3e64
Luis D. Pedrosa <luis.pedrosa@usc.edu>
2014-09-24 17:31:23 -0700

    Fixed octave script directory issue.

--- scripts/pc-depth.sh
+++ scripts/pc-depth.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+set -e
+
 spaSplitPaths -i $1 -o %06d.paths -p 1
 
 parallel -j 200% -k "grep '^(query \[(\|^         (' {} | grep -v '(Eq (Read w8 [0-9]* spa_in_msg_request)' | wc -l" ::: ??????.paths > pc-depth.csv
 rm ??????.paths
 
-octave -qf pc-depth-cdf.m
+octave -qf `dirname $BASH_SOURCE`/pc-depth-cdf.m
 rm pc-depth.csv
