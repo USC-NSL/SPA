4094700fb9a1b95604d6bca9b89b2970865faa9b
Luis D. Pedrosa <luis.pedrosa@usc.edu>
2015-02-02 14:17:59 -0800

    Removed basefiles feature from parallel-pic.sh Fixed race conditions in parallel-pic.sh.

--- scripts/parallel-pic.sh
+++ scripts/parallel-pic.sh
@@ -7,26 +7,16 @@ SERVER_BC="$2"
 CLIENT_PATHS="$3"
 SERVER_PATHS="$4"
 VALID_PATHS="$5"
-VALIDATION_WORK_DIR="$6"
+WORK_DIR="$6"
 VALIDATION_SCRIPT="$7"
 
-[ -r "$CLIENT_BC" ] \
-  || (echo "Error reading client bit-code: $CLIENT_BC."; exit 1)
-[ -r "$SERVER_BC" ] \
-  || (echo "Error reading server bit-code: $SERVER_BC."; exit 1)
+[ -n "$CLIENT_BC" ] || (echo "No client bit-code."; exit 1)
+[ -n "$SERVER_BC" ] || (echo "No server bit-code."; exit 1)
 [ -n "$CLIENT_PATHS" ] || (echo "No client path-file specified."; exit 1)
 [ -n "$SERVER_PATHS" ] || (echo "No server path-file specified."; exit 1)
 [ -n "$VALID_PATHS" ] || (echo "No validated path-file specified."; exit 1)
-[ -n "$VALIDATION_WORK_DIR" ] || (echo "No validation work directory."; exit 1)
+[ -n "$WORK_DIR" ] || (echo "No work directory."; exit 1)
 [ -n "$VALIDATION_SCRIPT" ] || (echo "No validation script."; exit 1)
-
-BASEFILES=""
-shift 7
-while [ "$1" != "" ]; do
-  BASEFILES="$BASEFILES --basefile $1"
-  [ -r "$1" ] || (echo "Error reading basefile: $1."; exit 1)
-  shift
-done
 
 TMPDIR="`mktemp -d`"
 CLIENT_PATHS_LIST="$TMPDIR/client-paths.list"
@@ -39,128 +29,83 @@ echo "Working in $TMPDIR."
 rm -f $CLIENT_PATHS $SERVER_PATHS $VALID_PATHS
 touch $CLIENT_PATHS $SERVER_PATHS $VALID_PATHS
 
-spa-pic -max-instruction-time=10 -max-solver-time=10 -max-time=7200 \
-  --client $CLIENT_BC --path-file $CLIENT_PATHS \
-  >$CLIENT_PATHS.log 2>&1 &
-CLIENT_PID=$!
+# Set up pipeline
+mkfifo $CLIENT_PATHS_LIST
+inotifywait -m -e CLOSE_WRITE --format '%w%f' $TMPDIR \
+  > $CLIENT_PATHS_LIST &
+CLIENT_LIST_PID=$!
+
+mkfifo $SERVER_PATHS_LIST
+inotifywait -q -m -e MOVED_TO --format '%w%f' $TMPDIR \
+  > $SERVER_PATHS_LIST &
+SERVER_LIST_PID=$!
+
+mkfifo $UNTESTED_PATHS_LIST
+inotifywait -q -m -e CLOSE_WRITE --format '%w%f' $TMPDIR \
+  > $UNTESTED_PATHS_LIST &
+UNTESTED_LIST_PID=$!
+
+mkfifo $VALID_PATHS_LIST
+inotifywait -q -m -e MOVED_TO --format '%w%f' $TMPDIR \
+  > $VALID_PATHS_LIST &
+VALID_LIST_PID=$!
 
 spaSplitPaths -i $CLIENT_PATHS -f -o "$TMPDIR/%08d-client.paths" -p 1 &
 CLIENT_SPLIT_PID=$!
 
-touch $CLIENT_PATHS_LIST
-(while [ 1 ]; do
-  kill -0 $CLIENT_SPLIT_PID 2>/dev/null || true $((DONE=1))
-  ls -1tr $(eval echo '$TMPDIR/*-client.paths') 2>/dev/null \
-    | diff -ed $CLIENT_PATHS_LIST - \
-    | tail -n +2 | head -n -1 >> $CLIENT_PATHS_LIST
-  if [ "$DONE" ]; then
-    echo "Finished collecting client paths."
-    sleep 1
-    rm $CLIENT_PATHS_LIST
-    exit 0
-  fi
-  sleep 1
-done) &
-CLIENT_LIST_PID=$!
-
-tail --follow=name $CLIENT_PATHS_LIST 2>/dev/null \
-  | parallel --progress --eta \
-      --sshloginfile .. --controlmaster \
-      --noswap -v --tag --linebuffer \
-      --basefile $SERVER_BC $BASEFILES \
-      --transfer --return "{.}-server.paths" --cleanup \
-      "echo Basefiles: $BASEFILES; \
-       echo Transfer: {}; echo Return: {.}-server.paths; echo ls:; ls; \
-       date '+Started: %s.%N (%c)'; \
-       spa/Release+Asserts/bin/spa-pic \
-         -max-instruction-time=10 -max-solver-time=10 -max-time=7200 \
-         --path-file {.}-server.paths -sender-paths {} --server $SERVER_BC; \
-       date '+Finished: %s.%N (%c)';" >$SERVER_PATHS.log 2>&1 &
+grep --line-buffered '/[0-9]*-client.paths$' $CLIENT_PATHS_LIST \
+  | parallel --progress \
+    --sshloginfile .. \
+    --noswap -v --tag --linebuffer \
+    --transfer --return "{.}-server.paths" --cleanup \
+    "cd $WORK_DIR; \
+    echo Transfer: {}; echo Return: {.}-server.paths; echo pwd: \$PWD; \
+    echo ls:; ls; \
+    date '+Started: %s.%N (%c)'; \
+    \$HOME/spa/Release+Asserts/bin/spa-pic \
+      -max-instruction-time=10 -max-solver-time=10 -max-time=7200 \
+      --path-file {.}-server.paths -sender-paths {} --server $SERVER_BC; \
+    date '+Finished: %s.%N (%c)';" >$SERVER_PATHS.log 2>&1 &
 SERVER_PID=$!
 
-touch $SERVER_PATHS_LIST
-(while [ 1 ]; do
-  kill -0 $SERVER_PID 2>/dev/null || true $((DONE=1))
-  ls -1tr $(eval echo '$TMPDIR/*-client-server.paths') 2>/dev/null \
-    | diff -ed $SERVER_PATHS_LIST - \
-    | tail -n +2 | head -n -1 >> $SERVER_PATHS_LIST
-  if [ "$DONE" ]; then
-    echo "Finished collecting server paths."
-    sleep 1
-    rm $SERVER_PATHS_LIST
-    exit 0
-  fi
-  sleep 1
-done) &
-SERVER_LIST_PID=$!
-
-tail --follow=name $SERVER_PATHS_LIST 2>/dev/null \
-  | parallel cat {} >> $SERVER_PATHS &
+grep --line-buffered '/[0-9]*-client-server.paths$' $SERVER_PATHS_LIST \
+  | parallel cat {} > $SERVER_PATHS &
 SERVER_JOIN_PID=$!
 
-spaSplitPaths -i $SERVER_PATHS -f -o "$TMPDIR/%08d-untested.paths" -p 10 &
+spaSplitPaths -i $SERVER_PATHS -f -o "$TMPDIR/%08d-untested.paths" -p 1 &
 SERVER_SPLIT_PID=$!
 
-touch $UNTESTED_PATHS_LIST
-(while [ 1 ]; do
-  kill -0 $SERVER_SPLIT_PID 2>/dev/null || true $((DONE=1))
-  ls -1tr $(eval echo '$TMPDIR/*-untested.paths') 2>/dev/null \
-    | diff -ed $UNTESTED_PATHS_LIST - \
-    | tail -n +2 | head -n -1 >> $UNTESTED_PATHS_LIST
-  if [ "$DONE" ]; then
-    echo "Finished collecting untested paths."
-    sleep 1
-    rm $UNTESTED_PATHS_LIST
-    exit 0
-  fi
-  sleep 1
-  done) &
-UNTESTED_LIST_PID=$!
-
-tail --follow=name $UNTESTED_PATHS_LIST 2>/dev/null \
-  | parallel --progress --eta \
-      --sshloginfile .. --controlmaster \
-      --noswap -v --tag --linebuffer \
-      --transfer --return "{.}-valid.paths" --cleanup \
-      "cd $VALIDATION_WORK_DIR; \
-      echo Transfer: {}; echo Return: {.}-valid.paths; echo pwd: \$PWD; \
-      echo ls:; ls; \
-      echo netns: spa{#}; \
-      echo user: \$USER; \
-      date '+Started: %s.%N (%c)'; \
-      sudo ip netns add spa{#}; \
-      sudo ip netns exec spa{#} ifconfig lo 127.0.0.1/8 up; \
-      sudo ip netns exec spa{#} sudo -u \$USER \
-        \$HOME/spa/Release+Asserts/bin/spa-validate -d \
-          {} {.}-valid.paths \
-          \"$VALIDATION_SCRIPT\"; \
-      sudo ip netns delete spa{#}; \
-      date '+Finished: %s.%N (%c)';" 2>&1 | tee $VALID_PATHS.log &
+grep --line-buffered '/[0-9]*-untested.paths$' $UNTESTED_PATHS_LIST \
+  | parallel --progress \
+    --sshloginfile .. \
+    --noswap -v --tag --linebuffer \
+    --transfer --return "{.}-valid.paths" --cleanup \
+    "cd $WORK_DIR; \
+    echo Transfer: {}; echo Return: {.}-valid.paths; echo pwd: \$PWD; \
+    echo ls:; ls; \
+    echo netns: spa{#}; \
+    echo user: \$USER; \
+    date '+Started: %s.%N (%c)'; \
+    sudo ip netns add spa{#}; \
+    sudo ip netns exec spa{#} ifconfig lo 127.0.0.1/8 up; \
+    sudo ip netns exec spa{#} sudo -u \$USER \
+      \$HOME/spa/Release+Asserts/bin/spa-validate -d \
+        {} {.}-valid.paths \
+        \"$VALIDATION_SCRIPT\"; \
+    sudo ip netns delete spa{#}; \
+    date '+Finished: %s.%N (%c)';" >$VALID_PATHS.log 2>&1 &
 VALIDATE_PID=$!
 
-touch $VALID_PATHS_LIST
-(while [ 1 ]; do
-  kill -0 $VALIDATE_PID 2>/dev/null || true $((DONE=1))
-  ls -1tr $(eval echo '$TMPDIR/*-valid.paths') 2>/dev/null \
-    | diff -ed $VALID_PATHS_LIST - \
-    | tail -n +2 | head -n -1 >> $VALID_PATHS_LIST
-  if [ "$DONE" ]; then
-    echo "Finished collecting valid paths."
-    sleep 1
-    rm $VALID_PATHS_LIST
-    exit 0
-  fi
-  sleep 1
-  done) &
-VALID_LIST_PID=$!
-
-tail --follow=name $VALID_PATHS_LIST 2>/dev/null \
-  | parallel cat {} >> $VALID_PATHS &
+grep --line-buffered '/[0-9]*-untested-valid.paths$' $VALID_PATHS_LIST \
+  | parallel cat {} > $VALID_PATHS &
 VALID_JOIN_PID=$!
 
 spa-cluster-manual -f $VALID_PATHS &
 CLUSTER_PID=$!
 
+sleep 1
+
+# Start running.
 (START_TIME="`date '+%s.%N'`"
 while [ 1 ]; do
   CUR_TIME="`date '+%s.%N'`"
@@ -176,30 +121,48 @@ while [ 1 ]; do
 done) &
 STATS_PID=$!
 
+spa-pic -max-instruction-time=10 -max-solver-time=10 -max-time=7200 \
+  --client $CLIENT_BC --path-file $CLIENT_PATHS \
+  >$CLIENT_PATHS.log 2>&1 &
+CLIENT_PID=$!
+
 # Flush the pipeline
-wait $CLIENT_PID
+wait $CLIENT_PID || true
+echo "Client analysis complete."
 sleep 1
 kill $CLIENT_SPLIT_PID
 wait $CLIENT_SPLIT_PID 2>/dev/null || true
-wait $CLIENT_LIST_PID
-wait $SERVER_PID
-wait $SERVER_LIST_PID
-wait $SERVER_JOIN_PID
+kill $CLIENT_LIST_PID
+wait $CLIENT_LIST_PID 2>/dev/null || true
+wait $SERVER_PID || true
+echo "Server analysis complete."
+kill $SERVER_LIST_PID
+wait $SERVER_LIST_PID 2>/dev/null || true
+wait $SERVER_JOIN_PID || true
 sleep 1
 kill $SERVER_SPLIT_PID
 wait $SERVER_SPLIT_PID 2>/dev/null || true
-wait $UNTESTED_LIST_PID
-wait $VALIDATE_PID
-wait $VALID_LIST_PID
-wait $VALID_JOIN_PID
-sleep 1
-kill $CLUSTER_PID
-wait $CLUSTER_PID 2>/dev/null || true
-sleep 1
-kill $STATS_PID
-wait $STATS_PID 2>/dev/null || true
+kill $UNTESTED_LIST_PID
+wait $UNTESTED_LIST_PID 2>/dev/null || true
+wait $VALIDATE_PID || true
+echo "Validation analysis complete."
+kill $VALID_LIST_PID
+wait $VALID_LIST_PID 2>/dev/null || true
+wait $VALID_JOIN_PID || true
 
-echo "Cleaning up."
-rm -rf $TMPDIR
+function clean_up {
+  kill $CLUSTER_PID
+  wait $CLUSTER_PID 2>/dev/null || true
+  sleep 1
+  kill $STATS_PID
+  wait $STATS_PID 2>/dev/null || true
+
+  echo "Cleaning up."
+  rm -rf $TMPDIR
+}
+trap clean_up SIGINT SIGTERM
+
+sleep 1
+clean_up
 
 echo "Done."
