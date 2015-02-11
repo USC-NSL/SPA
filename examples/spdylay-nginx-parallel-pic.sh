b70c6ca18d16f1c1689c2ab11a7661c6aad4313c
Luis D. Pedrosa <luis.pedrosa@usc.edu>
2015-02-10 23:49:32 -0800

    Craeted spdylay-nginx-parallel-pic.sh.

---
+++ examples/spdylay-nginx-parallel-pic.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+set -e
+
+parallel-pic.sh \
+  ../spdylay/spa-client.bc out/llvm/objs/nginx \
+  'spa/examples/nginx' \
+  '../spdylay/spa-client.e2etest.exe' 'out/test/objs/nginx' 8000 \
+  spdylay.paths spdylay-nginx.paths spdylay-nginx-valid.paths \
+  2>&1 | tee spdylay-nginx.log
