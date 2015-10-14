bfc4346b1d1bbea843aee144bfcdc8e96dca07e0
Luis D. Pedrosa <luis.pedrosa@usc.edu>
2015-10-14 15:26:51 -0700

    Log output.

--- examples/kv/conversational-analysis.sh
+++ examples/kv/conversational-analysis.sh
@@ -5,4 +5,5 @@ spa-explore \
     --out-paths kv-client.paths \
     --start-from spa_entry \
     --output-at spa_msg_output_point \
-    kv-client.bc
+    kv-client.bc \
+    2>&1 | tee kv-client.paths.log
