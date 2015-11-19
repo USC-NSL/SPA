df988aa47f05984a155af13b739556f2f731bd77
Luis D. Pedrosa <luis.pedrosa@usc.edu>
2015-11-18 21:36:08 -0800

    Disabled sanity check in pic to fix lazy loading.

--- tools/pic/pic.cpp
+++ tools/pic/pic.cpp
@@ -355,15 +355,15 @@ int main(int argc, char **argv, char **envp) {
   // Create instruction filter.
   klee::klee_message("   Creating CFG filter.");
   SPA::CFGBackwardIF *filter = new SPA::CFGBackwardIF(cfg, cg, checkpoints);
-  for (auto it : entryPoints) {
-    if (!filter->checkInstruction(it)) {
-      klee::klee_message(
-          "Entry point at function %s is not included in filter. Disabling.",
-          it->getParent()->getParent()->getName().str().c_str());
-      //       assert(false && "Entry point not included in filter.");
-      filter = NULL;
-    }
-  }
+//   for (auto it : entryPoints) {
+//     if (!filter->checkInstruction(it)) {
+//       klee::klee_message(
+//           "Entry point at function %s is not included in filter. Disabling.",
+//           it->getParent()->getParent()->getName().str().c_str());
+//       assert(false && "Entry point not included in filter.");
+//       filter = NULL;
+//     }
+//   }
   //   if (filter) {
   //     if (Client)
   //       spa.addStateUtilityBack(filter, false);
