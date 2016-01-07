0b8d0ca81746802a603b38283990576a55228895
Luis D. Pedrosa <luis.pedrosa@usc.edu>
2016-01-07 14:54:03 -0800

    Fixed issues with jobs using the same input (and parallel deleting it). Generate bogus workload to get parallel started. Fixed warning when no output file (from bogus input).

--- scripts/parallel-conversational-analysis.sh
+++ scripts/parallel-conversational-analysis.sh
@@ -28,22 +28,24 @@ rm -f $PATH_FILE
 touch $PATH_FILE
 
 # Set up pipeline
-spaSplitPaths -i $PATH_FILE -f -o "$TMPDIR/%08d.paths" -p 1 &
-
 for i in ${!OPTS[@]}; do
   rm -f participant-$i.log
+
+  spaSplitPaths -i $PATH_FILE -f -o "$TMPDIR/%08d-$i.paths" -p 1 &
+
   (cd $TMPDIR; \
   inotifywait -q -m -e CLOSE_WRITE --format '%w%f' . \
-    | grep --line-buffered -- '/[0-9]*.paths$' \
+    | grep --line-buffered -- "/[0-9]*-$i.paths$" \
     | parallel --progress \
       --sshloginfile .. --controlmaster \
       -v --tag --linebuffer \
-      --transfer --return "{.}-$i-result.paths" --cleanup \
-      "echo Transfer: {}; echo Return: {.}-$i-result.paths; echo pwd: \$PWD; \
+      --transfer --return "{.}-result.paths" --cleanup \
+      "echo Transfer: {}; echo Return: {.}-result.paths; echo pwd: \$PWD; \
       echo ls:; ls; \
       date '+Started: %s.%N (%c)'; \
+      touch {.}-result.paths; \
       test -s {} && \$HOME/spa/Release+Asserts/bin/spa-explore \
-        --in-paths {} --dont-load-empty-path --out-paths {.}-$i-result.paths \
+        --in-paths {} --dont-load-empty-path --out-paths {.}-result.paths \
         ${OPTS[i]}; \
       date '+Finished: %s.%N (%c)';") | tee -a participant-$i.log &
 done
@@ -54,7 +56,8 @@ inotifywait -q -m -e MOVED_TO --format '%w%f' $TMPDIR \
 
 # Start root paths
 for i in ${!OPTS[@]}; do
-  (cd $TMPDIR; echo root-$i \
+  touch $TMPDIR/root-$i.paths
+  (cd $TMPDIR; echo root-$i.paths \
     | parallel --progress \
       --sshloginfile .. --controlmaster \
       -v --tag --linebuffer \
@@ -63,8 +66,16 @@ for i in ${!OPTS[@]}; do
       echo ls:; ls; \
       date '+Started: %s.%N (%c)'; \
       \$HOME/spa/Release+Asserts/bin/spa-explore \
-        --out-paths {.}-result.paths ${OPTS[i]}; \
+        --in-paths {} --out-paths {.}-result.paths ${OPTS[i]}; \
       date '+Finished: %s.%N (%c)';") | tee -a participant-$i.log &
+done
+
+# Generate bogus workload to fill up parallel input buffer.
+sleep 2
+for j in `seq -f %03.0f 1 32`; do
+  for i in ${!OPTS[@]}; do
+    touch $TMPDIR/$j-$i.paths
+  done
 done
 
 trap 'kill $(jobs -p)' SIGINT
