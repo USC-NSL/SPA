896014a64bb51b31e8dda6bc347fd2ecfe59ac7e
Luis D. Pedrosa <luis.pedrosa@usc.edu>
2016-02-18 11:43:16 -0800

    Added tool to join path files (more consistently and flexibly than cat).

---
+++ tools/spaJoinPaths/spaJoinPaths.cpp
@@ -0,0 +1,50 @@
+#include <unistd.h>
+#include <set>
+
+// FIXME: Ugh, this is gross. But otherwise our config.h conflicts with LLVMs.
+#undef PACKAGE_BUGREPORT
+#undef PACKAGE_NAME
+#undef PACKAGE_STRING
+#undef PACKAGE_TARNAME
+#undef PACKAGE_VERSION
+
+#include "spa/PathLoader.h"
+
+#define FOLLOW_WAIT_S 1
+
+int main(int argc, char **argv, char **envp) {
+  assert(argc > 1 && "Insufficient arguments.");
+  bool followInputs = (strcmp(argv[1], "-f") == 0);
+  assert(argc > (followInputs ? 3 : 2) && "Insufficient arguments.");
+
+  std::set<SPA::PathLoader *> inputs;
+  for (int i = (followInputs ? 2 : 1); i < argc - 1; i++) {
+    std::ifstream *ifs = new std::ifstream(argv[i]);
+    assert(ifs->good() && "Unable to open input path-file.");
+    inputs.insert(new SPA::PathLoader(*ifs));
+  }
+
+  std::ofstream output(argv[argc - 1]);
+  assert(output.good() && "Unable to open output path-file.");
+
+  unsigned long pathCount = 0;
+  bool foundPath;
+  do {
+    foundPath = false;
+    for (auto iit : inputs) {
+      std::string p = iit->getPathText();
+      if (p.empty()) {
+        continue;
+      } else {
+        foundPath = true;
+        output << p;
+        pathCount++;
+      }
+    }
+    if (followInputs && !foundPath) {
+      sleep(FOLLOW_WAIT_S);
+    }
+  } while (foundPath || followInputs);
+
+  llvm::outs() << "Joined " << pathCount << " paths.\n";
+}
