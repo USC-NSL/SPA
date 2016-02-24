cb9258aeddce01806558eb354b1062b71b055fa7
Luis D. Pedrosa <luis.pedrosa@usc.edu>
2016-02-23 19:08:32 -0800

    Enable directed conversation exploration in KV analysis script. Embed documentation web-server in KV analysis script.

--- examples/kv/distributed-conversational-analysis-udp.sh
+++ examples/kv/distributed-conversational-analysis-udp.sh
@@ -2,11 +2,16 @@
 
 set -e
 
+TARGET_CONVERSATION="kv-client kv-server-1 kv-server-2 kv-server-3 kv-client"
+
+trap "trap - SIGTERM && kill -- -$$" SIGINT SIGTERM EXIT
+
 distributed-conversational-analysis.sh kv.paths \
-  "--shallow-exploration --connect-sockets --start-from spa_entry  --toward kv_done                --output-at spa_msg_no_input_point --output-at kv_done --participant kv-client   --ip 127.0.0.1 spa/examples/kv/kv-client-udp.bc; \
-   --shallow-exploration --connect-sockets --start-from spa_entry1 --toward spa_msg_no_input_point --output-at spa_msg_no_input_point                     --participant kv-server-1 --ip 127.0.0.2 spa/examples/kv/kv-server-udp.bc; \
-   --shallow-exploration --connect-sockets --start-from spa_entry2 --toward spa_msg_no_input_point --output-at spa_msg_no_input_point                     --participant kv-server-2 --ip 127.0.0.3 spa/examples/kv/kv-server-udp.bc; \
-   --shallow-exploration --connect-sockets --start-from spa_entry3 --toward spa_msg_no_input_point --output-at spa_msg_no_input_point                     --participant kv-server-3 --ip 127.0.0.4 spa/examples/kv/kv-server-udp.bc" &
+  "--shallow-exploration --connect-sockets --start-from spa_entry  --toward kv_done                --output-at spa_msg_no_input_point --output-at kv_done --towards-conversation \"$TARGET_CONVERSATION\" --participant kv-client   --ip 127.0.0.1 spa/examples/kv/kv-client-udp.bc; \
+   --shallow-exploration --connect-sockets --start-from spa_entry1 --toward spa_msg_no_input_point --output-at spa_msg_no_input_point                     --towards-conversation \"$TARGET_CONVERSATION\" --participant kv-server-1 --ip 127.0.0.2 spa/examples/kv/kv-server-udp.bc; \
+   --shallow-exploration --connect-sockets --start-from spa_entry2 --toward spa_msg_no_input_point --output-at spa_msg_no_input_point                     --towards-conversation \"$TARGET_CONVERSATION\" --participant kv-server-2 --ip 127.0.0.3 spa/examples/kv/kv-server-udp.bc; \
+   --shallow-exploration --connect-sockets --start-from spa_entry3 --toward spa_msg_no_input_point --output-at spa_msg_no_input_point                     --towards-conversation \"$TARGET_CONVERSATION\" --participant kv-server-3 --ip 127.0.0.4 spa/examples/kv/kv-server-udp.bc" &
+SPA_PID=$!
 
 sleep 1
 
@@ -16,6 +21,15 @@ spa-explore-conversation \
     --follow-in-paths \
     kv.paths >/dev/null 2>&1 &
 
-trap "trap - SIGTERM && kill -- -$$" SIGINT SIGTERM EXIT
+echo "Follow analysis progress on: http://localhost:8000/"
+spa-doc --serve-http 8000 \
+    --map-src /home/lpedrosa/spa=/home/david/Projects/spa \
+    --color-filter "lightgreen:REACHED kv_done" \
+    --color-filter "cyan:CONVERSATION $TARGET_CONVERSATION" \
+    kv.paths &
+
+wait $SPA_PID
+
+echo "Analysis is complete. Ctrl-C when ready."
 
 wait
